d9247c58375d04a0b1b09039c8194ec7ccb06eda
rquintas <rquintas@rquintas.net>
2012-03-02 22:39:17 +0000

    Improved

--- Chapter1/ex1_3.rkt
+++ Chapter1/ex1_3.rkt
@@ -1,18 +1,17 @@
-(define (largest a b c)
-  (if (and (> a b) (> a c)) a
-      (if (and (> b a) (> b c)) b
-          c)))
-
-(define (2ndlargest a b c)
-  (if (or (and (>= a b) (<= a c)) (and (<= a b) (>= a c))) a
-      (if (or (and (>= b a) (<= b c)) (and (<= b a) (>= b c))) b
-          c)))
-
+;Define a procedure that takes three numbers as 
+;arguments and returns the sum of the squares of 
+;the two larger numbers.
 
 (define (square x)
   (* x x))
 
 (define (multiply2 x y z)
-  (* (square (largest x y z)) (square (2ndlargest x y z))))
+  (if (> x y)
+      (if (> y z)
+          (+ (square x) (square y))
+          (+ (square x) (square z)))
+      (if (> x z)
+          (+ (square x) (square y))
+          (+ (square y) (square z)))))
 
 (multiply2 2 2 4)
